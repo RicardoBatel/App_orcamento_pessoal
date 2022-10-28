d3adadb52fcd5bd24e1c4fbcd89532a5fdf89f76
RicardoBatel <112258124+RicardoBatel@users.noreply.github.com>
2022-10-28 12:35:44 -0300

    Removendo despesas

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ class Bd {
                 continue
             }
 
+            despesa.id = i
             despesas.push(despesa)
         }
         return despesas
@@ -107,6 +108,10 @@ class Bd {
         }
 
         return despesasFiltradas
+    }
+
+    remover(id) {
+        localStorage.removeItem(id)
     }
 }
 
@@ -203,6 +208,25 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criar o botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = 'id_despesa_' + d.id
+        btn.onclick = function() {
+
+            //remover despesa
+            let id = this.id.replace('id_despesa_', '')
+
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
+
     })
 
 }
